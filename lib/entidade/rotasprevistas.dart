afe0bd544b0903848c00c529d05d687f8144ede6
victorhugonicolau <victor.castro29@etec.sp.gov.br>
2024-09-11 16:08:51 -0300

    RastroBus - Formatando e criando o tema + google fontes - Wed, Sep 11, 2024  4:08:51 PM

---
+++ lib/entidade/rotasprevistas.dart
@@ -0,0 +1,11 @@
+class RotasPrevistas {
+  final String id;
+  final int numPonto;
+  final String nome;
+
+  RotasPrevistas({
+    required this.nome,
+    required this.id,
+    required this.numPonto,
+  });
+}
