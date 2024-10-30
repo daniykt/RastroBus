2798e2396e858aea17f0cadd72d01eff4a7056ef
LeonardoPauli-cps <leonardo.santos1095@etec.sp.gov.br>
2024-10-29 22:41:40 -0300

    Branch do Leo 29/10 - Pontos puxados da api

--- lib/componentes/mapa.dart
+++ lib/componentes/mapa.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
-import 'package:rastrobus/entidade/ponto.dart';
+import 'package:provider/provider.dart';
+import 'package:rastrobus/vm/rotasprevistas_vm.dart';
 
 class Mapa extends StatefulWidget {
   const Mapa({super.key});
@@ -11,46 +12,6 @@ class Mapa extends StatefulWidget {
 
 class _MapaState extends State<Mapa> with AutomaticKeepAliveClientMixin {
   late MapController controller;
-  
-  // Exemplo de lista de pontos (você pode obter isso de uma API ou banco de dados)
-  final List<Ponto> pontos = [
-    Ponto(id: 1,
-    numero: 101,
-    cor: "#008000",
-    endereco: "Avenida Baldan, próximo ao Supermercado São Judas Tadeu",
-    bairro: "Centro",
-    latitude: -21.605985,
-    longitude: -48.366354,
-    imagem: "aa"),
-
-    Ponto(id: 2,
-    numero: 102,
-    cor: "#0000FF",
-    endereco: "Rua Prudente de Moraes, esquina com a Rua João Pessoa",
-    bairro: "Centro",
-    latitude: -21.608321,
-    longitude: -48.365489,
-    imagem: "aa"),
-
-    Ponto(id: 3,
-    numero: 103,
-    cor: "#FF0000",
-    endereco: "Avenida Trolesi, próximo ao Matão Center",
-    bairro: "Imperador",
-    latitude: -21.606428,
-    longitude: -48.36291,
-    imagem: "aa"),
-
-    Ponto( id: 4,
-    numero: 104,
-    cor: "#008000",
-    endereco: "Rua Rui Barbosa, em frente à Praça Alfredo de Paiva Garcia",
-    bairro: "Centro",
-    latitude: -21.608853,
-    longitude: -48.368657,
-    imagem: "aa"),
-    // Adicione mais pontos conforme necessário
-  ];
 
   @override
   void initState() {
@@ -61,22 +22,6 @@ class _MapaState extends State<Mapa> with AutomaticKeepAliveClientMixin {
         unFollowUser: false,
       ),
     );
-
-    // Adicionando pontos no mapa
-    Future.delayed(const Duration(seconds: 1), () {
-      for (var ponto in pontos) {
-        controller.addMarker(
-          GeoPoint(latitude: ponto.latitude, longitude: ponto.longitude),
-          markerIcon: const MarkerIcon(
-            icon: Icon(
-              Icons.location_on,
-              color: Colors.red, // Cor dinâmica
-              size: 40,
-            ),
-          ),
-        );
-      }
-    });
   }
 
   @override
@@ -84,6 +29,26 @@ class _MapaState extends State<Mapa> with AutomaticKeepAliveClientMixin {
   Widget build(BuildContext context) {
     super.build(context);
     final tema = Theme.of(context);
+
+    final vm = Provider.of<RotasPrevistasVIewModel>(context);
+    final points = <StaticPositionGeoPoint>[];
+
+    // Adicionando pontos no mapa
+    for (var ponto in vm.rotasprevistas) {
+      points.add(
+        StaticPositionGeoPoint(
+          ponto.id.toString(),
+          const MarkerIcon(
+            icon: Icon(
+              Icons.location_pin,
+              color: Colors.red,
+              size: 40,
+            ),
+          ),
+          [GeoPoint(latitude: ponto.latitude, longitude: ponto.longitude)],
+        ),
+      );
+    }
 
     return OSMFlutter(
       controller: controller,
@@ -95,6 +60,7 @@ class _MapaState extends State<Mapa> with AutomaticKeepAliveClientMixin {
           maxZoomLevel: 19,
           stepZoom: 1.0,
         ),
+        staticPoints: points,
         userLocationMarker: UserLocationMaker(
           personMarker: MarkerIcon(
             icon: Icon(
